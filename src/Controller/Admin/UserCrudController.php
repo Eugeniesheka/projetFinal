37d6d9587651de9a237f37ef5c5edf4623096913
Eugenie SHEKA KASONGO <eugenie.sheka-kasongo@etu.univ-nantes.fr>
2022-03-25 20:39:03 +0100

    Ajout inscription

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AvatarField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id')->onlyOnDetail();
+        yield AvatarField::new('email')->setIsGravatarEmail()->hideOnForm();
+        yield TextField::new('fullName');
+        yield TextField::new('username');
+        yield EmailField::new('email');
+    }
+}
